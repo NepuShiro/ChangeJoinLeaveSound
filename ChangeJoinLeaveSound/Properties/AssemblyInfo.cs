4ca9c3a6de25ba9c37cc9a412514b5e3008962ae
NepuShiro <nepushiro@nepunep.xyz>
2024-11-26 20:31:03 -0600

    Update AssemblyInfo

--- ChangeJoinLeaveSound/Properties/AssemblyInfo.cs
+++ ChangeJoinLeaveSound/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("ChangeJoinLeaveSound")]
 [assembly: AssemblyProduct("ChangeJoinLeaveSound")]
-[assembly: AssemblyDescription("Description of your mod")]
-[assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
+[assembly: AssemblyDescription("Allows you to change the default User Join/Leave sounds, and color")]
+[assembly: AssemblyCopyright("Copyright © 2024 NepuShiro")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
